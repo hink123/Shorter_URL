6fa7fede599ecb15eb441a894a44f982c43407fa
hink123 <will.hink24@gmail.com>
2022-04-30 15:31:49 -0700

    added validtion to controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const { base } = require('../models/url');
 const Url = require('../models/url');
 const ALPHABET = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const VALID_URL_REGEX = /^(http(s)?:\/\/).*/;
+const VALID_SHORT_URL = /^(http:\/\/wh\.ly\/)[a-z]{5}/;
 
 module.exports = {
     shortenUrl,
@@ -12,31 +14,45 @@ async function shortenUrl(req, res) {
         let baseUrl = req.body.url;
         let shortUrl = 'http://wh.ly/';
         let randomNumb;
+        let validUrl = isValidUrl(baseUrl, VALID_URL_REGEX);
 
-        for(let i = 0; i < 5; i++) {
-            randomNumb = Math.floor(Math.random()*ALPHABET.length);
-            shortUrl += ALPHABET[randomNumb];
+        if(validUrl) {
+            for(let i = 0; i < 5; i++) {
+                randomNumb = Math.floor(Math.random()*ALPHABET.length);
+                shortUrl += ALPHABET[randomNumb];
+            }
+            await url.save()
+            return res.render('index', {shortUrl, baseUrl: null, err: null});
+        } else {
+            return res.render('index', {shortUrl: null, baseUrl: null, err: 'Invalid URL'});
         }
-
-        let url = new Url({baseUrl, shortUrl});
-        await url.save()
-        res.render('index', {shortUrl, baseUrl: null});
     }
      catch (err) {
-        console.log('Oops something went wrong!')
-        res.json({err});
+        return res.json({err});
     }
 }
 
 async function lengthenUrl(req, res) {
     try {
         let shortUrl = req.query.url;
+        let validUrl = isValidUrl(shortUrl, VALID_SHORT_URL);
         let urlDoc = await Url.findOne({shortUrl});
-        let baseUrl = urlDoc.baseUrl;
-        res.render('index', {baseUrl, shortUrl: null});
+
+        if(!validUrl) {
+            return res.render('index', {baseUrl: null, shortUrl: null, err: 'Invalid Short URL'});
+        }
+        if(!urlDoc) {
+            return res.render('index', {baseUrl: null, shortUrl: null, err: 'Base URL Not Found'});
+        } else {
+            let baseUrl = urlDoc.baseUrl;
+            return res.render('index', {baseUrl, shortUrl: null, err: null});
+        }
     } 
     catch (err) {
-        console.log('Oops something went wrong!')
-        res.json({err});
+        return res.json({err});
     }
+}
+
+function isValidUrl(url, regex) {
+    return url.match(regex);
 }
