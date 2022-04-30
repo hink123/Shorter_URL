4f76b0e0bf7c74c0cb476bf5ae7a2bdeda186a24
hink123 <will.hink24@gmail.com>
2022-04-29 18:57:11 -0700

    created shorten and lengthen controllers

--- controllers/index.js
+++ controllers/index.js
@@ -1,21 +1,41 @@
+const { base } = require('../models/url');
+const Url = require('../models/url');
+const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
 module.exports = {
     shortenUrl,
     lengthenUrl
 }
 
-function shortenUrl(req, res) {
+async function shortenUrl(req, res) {
     try {
+        let baseUrl = req.body.url;
+        let shortUrl = 'http://wh.ly/';
+        let randomNumb;
 
-    } catch (err) {
+        for(let i = 0; i < 5; i++) {
+            randomNumb = Math.floor(Math.random()*alphabet.length);
+            shortUrl += alphabet[randomNumb];
+        }
+
+        let url = new Url({baseUrl, shortUrl});
+        await url.save()
+        res.render('index');
+    }
+     catch (err) {
         console.log('Oops something went wrong!')
         res.json({err});
     }
 }
 
-function lengthenUrl(req, res) {
+async function lengthenUrl(req, res) {
     try {
-
-    } catch (err) {
+        let shortUrl = req.body.url;
+        let urlDoc = await Url.findOne({shortUrl});
+        let baseUrl = urlDoc.baseUrl;
+        res.render('index');
+    } 
+    catch (err) {
         console.log('Oops something went wrong!')
         res.json({err});
     }
