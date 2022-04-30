dc661a37799a8b26b64c48507c82aa8d7b334f81
hink123 <will.hink24@gmail.com>
2022-04-29 18:58:13 -0700

    created the two routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const indexCtrl = require('../controllers/index');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/shorten', indexCtrl.shortenUrl);
+router.post('/lengthen', indexCtrl.lengthenUrl);
 
 module.exports = router;
