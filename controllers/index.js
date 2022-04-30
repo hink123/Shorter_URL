e1a5abddfec2fc1daa0c831a15738457d30e379e
hink123 <will.hink24@gmail.com>
2022-04-29 17:07:55 -0700

    created controller file

---
+++ controllers/index.js
@@ -0,0 +1,22 @@
+module.exports = {
+    shortenUrl,
+    lengthenUrl
+}
+
+function shortenUrl(req, res) {
+    try {
+
+    } catch (err) {
+        console.log('Oops something went wrong!')
+        res.json({err});
+    }
+}
+
+function lengthenUrl(req, res) {
+    try {
+
+    } catch (err) {
+        console.log('Oops something went wrong!')
+        res.json({err});
+    }
+}
